021c0df557f5087f4e0e89b4bee764d7fbb93a52
Rafael <luizhenrique157123@gmail.com>
2026-09-18 15:57:21 -0300

    Atualização do site

--- elohin lanches/script.js
+++ elohin lanches/script.js
@@ -1,7 +1,7 @@
 const whatsapp = 'https://wa.me/5583993443315?text=';
 const products = [
   { name: 'Quentinha', category: 'quentinhas', price: 'A partir de R$ 20', description: 'Duas opções de carne e acompanhamentos variados.', image: '../quentinhas/quentinha.jpg' },
-  { name: 'Bolo no pote', category: 'sobremesas', price: 'R$ 5,00', description: 'Bolo no pote feito com carinho.', image: 'a1f293b6-7e7b-498f-a6d0-e71243b30597.jpeg' },
+  { name: 'Bolo no pote', category: 'sobremesas', price: 'R$ 5,00', description: 'Sabores: MARACUJÁ, CHOCOLATE, NINHO COM NUTELLA e 2 AMORES.', image: 'a1f293b6-7e7b-498f-a6d0-e71243b30597.jpeg' },
   { name: 'Coxinha especial', category: 'salgados', price: 'R$ 8,00', description: 'Frango com requeijão ou carne seca.', image: '3db0ee47-e377-4dd5-9fd3-8263ed0e6a7a.jpeg' },
   { name: 'Mini vulcão', category: 'sobremesas', price: 'R$ 15,00', description: 'Uma sobremesa para deixar o dia melhor.', image: '358c2c0c-e200-4845-813a-d24a7eb2e7f5.jpeg' },
   { name: 'Lasanha bolonhesa', category: 'lanches', price: 'R$ 20,00', description: 'Lasanha bolonhesa de 500g.', image: '845081ca-ec4e-4c5b-baf6-095c70dd90b0.jpeg' },
